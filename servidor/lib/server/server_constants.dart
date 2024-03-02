fb1732fc0ced7046014123955e24346b61b83097
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-03-02 18:17:06 -0300

    Contants finalizado

---
+++ servidor/lib/server/server_constants.dart
@@ -0,0 +1,9 @@
+class ServerConfig {
+  static const String serverName = 'Phoenix Game Server';
+
+  static const int maxPlayers = 2;
+
+  static const String serverHost = '127.0.0.1';
+
+  static const int serverPort = 8090;
+}
