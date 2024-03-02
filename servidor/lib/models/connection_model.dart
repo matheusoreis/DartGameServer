9dbdabb0d2318ef30c557a67614a68fcdebdde6a
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-03-02 18:41:27 -0300

    ConnectionModel finalizado

--- servidor/lib/models/connection_model.dart
+++ servidor/lib/models/connection_model.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:servidor/server/server_memory.dart';
+
 /// modelos de dados de conexão do cliente.
 ///
 /// Este modelo representa uma instância de cliente de rede.
@@ -12,4 +14,14 @@ interface class ConnectionModel {
 
   /// Socket do cliente.
   final Socket socket;
+
+  /// Verifica se um determinado índice de slot está atualmente conectado.
+  ///
+  /// Este método retorna verdadeiro se o slot especificado estiver ocupado por um cliente conectado,
+  /// caso contrário, retorna falso.
+  ///
+  /// Retorna verdadeiro se o slot estiver ocupado, falso caso contrário.
+  bool isConnected() {
+    return !ServerMemory().clientConnections.isSlotEmpty(id);
+  }
 }
