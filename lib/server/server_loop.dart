9f585554653685bf0e4ca478100fcd6a0145bfbb
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-06-06 19:12:20 -0300

    ServerLoop com isolates

--- lib/server/server_loop.dart
+++ lib/server/server_loop.dart
@@ -1,13 +1,18 @@
+import 'dart:isolate';
 import 'package:servidor/server/server_globals.dart';
 
 class ServerLoop {
   static Future<void> start() async {
+    await Isolate.spawn(_loop, null);
+  }
+
+  static void _loop(_) {
     while (ServerGlobals.serverOpen) {
       final tick = DateTime.now().millisecondsSinceEpoch;
 
       print(tick);
 
-      await Future.delayed(const Duration(milliseconds: 1));
+      Future.delayed(const Duration(milliseconds: 1));
     }
   }
 }
